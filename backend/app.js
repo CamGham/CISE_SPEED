258f03701d013230fa66572dce139092223bef78
CamGham <greayham@gmail.com>
2022-09-23 11:02:56 +1200

    fixed db connection

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('../config/db');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello World!'));
+// Connect Database
+connectDB();
 
-const port  = process.env.PORT || 8082;
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ config/db.js
@@ -0,0 +1,24 @@
+  // db.js
+  
+  const mongoose = require('mongoose');
+  const config = require('config');
+  const db = config.get('mongoURI');
+  
+  const connectDB = async () => {
+    try {
+      await mongoose.connect(
+        db,
+        {
+          useNewUrlParser: true
+        }
+      );
+  
+      console.log('MongoDB is Connected...');
+    } catch (err) {
+        console.log("ERROR OPENING DB")
+      console.error(err.message);
+      process.exit(1);
+    }
+  };
+  
+  module.exports = connectDB;
