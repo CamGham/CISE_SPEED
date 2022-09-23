065523d725978457abfc9785293794f2d451d00a
Ivan Czar <ivanczar2013@gmail.com>
2022-09-23 22:41:01 +1200

    Added eslint and airbnb rules

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('../config/db');
-var cors = require('cors');
 
 const app = express();
 
-//Port
+// Port
 const port = process.env.PORT || 8082;
 
-//Cors
+// Cors
 app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
@@ -15,15 +15,14 @@ app.use(express.json({ extended: false }));
 
 // app.get('/', (req, res) => res.send('Hello world2!'));
 
-//Connect Database
+// Connect Database
 connectDB();
 if (process.env.NODE_ENV === 'production') {
   // serve front-end client from build folder
-  app.use(express.static(__dirname+'./../front-end/build'));
-  app.get('*', (req, res) =>{
-    res.sendFile(__dirname+'./../front-end/build/index.html')
+  app.use(express.static(`${__dirname}./../front-end/build`));
+  app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}./../front-end/build/index.html`);
   });
-  
 } else {
   app.get('/', (req, res) => res.send(`API running on port ${port}`));
 }
--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 // db.js
-
 const mongoose = require('mongoose');
 const config = require('config');
+
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
