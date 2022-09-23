1ecf369ef97a55453ef4a77a89acdb7cf9d5b1ff
CamGham <greayham@gmail.com>
2022-09-23 13:00:33 +1200

    heroku nav fixed

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,6 @@ const app = express();
 //Port
 const port = process.env.PORT || 8082;
 
-// Connect Database
-connectDB();
-
 //Cors
 app.use(cors({ origin: true, credentials: true }));
 
@@ -18,14 +15,13 @@ app.use(express.json({ extended: false }));
 
 // app.get('/', (req, res) => res.send('Hello world2!'));
 
-
-
+//Connect Database
 connectDB();
 if (process.env.NODE_ENV === 'production') {
   // serve front-end client from build folder
-  app.use(express.static(__dirname+'/front-end/build'));
+  app.use(express.static(__dirname+'../front-end/build'));
   app.get('*', (req, res) =>{
-    res.sendFile(__dirname+'/front-end/build/index.html')
+    res.sendFile(__dirname+'../front-end/build/index.html')
   });
   
 } else {
