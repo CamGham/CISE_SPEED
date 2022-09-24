f5ed9095ae88398ea35cfdacd5d9585f16973061
CamGham <greayham@gmail.com>
2022-09-25 12:35:51 +1300

    simple nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Submission from "./pages/Submission";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Submission/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/createArticle" element={<Submission/>}/>
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+        <h2>SPEED</h2>
+        <div>
+        <Link to="/showArticles">
+            Show Articles
+        </Link>
+        </div>
+        <div>
+        <Link to="/createArticle">
+            Create an Article
+        </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
