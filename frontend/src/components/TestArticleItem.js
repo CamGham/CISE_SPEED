bf40387ab9d555dabc30e7b21a6fa7870a9edfd6
CamGham <greayham@gmail.com>
2022-09-26 11:06:51 +1300

    console log get

--- frontend/src/components/TestArticleItem.js
+++ frontend/src/components/TestArticleItem.js
@@ -3,6 +3,7 @@ import React from 'react'
 const TestArticleItem = (props) => {
     const article = props.article;
     // const key = props.key;
+    console.log(article.title);
   return (
     <div>
         <h3>{article.title}</h3>
--- frontend/src/pages/ArticleDisplay.js
+++ frontend/src/pages/ArticleDisplay.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import TestArticleItem from '../components/TestArticleItem';
 
 const ArticleDisplay = () => {
     const [articles, setArticles] = useState([]);
-
+    let articleList;
 
     const getArticles = async () =>{
-        axios.get('http://localhost:8082/api/articles')
+        await axios
+        .get('http://localhost:8082/api/articles')
         .then(res => {
-            setArticles(res.data);
             console.log(res.data);
+            setArticles(res.data);
+            // console.log("DATA: " + res);
+            // console.log("ARTICLES: " + articles);
         })
         .catch(err => {
             console.log("error");
@@ -17,16 +21,38 @@ const ArticleDisplay = () => {
     }
 
 
-    let articleList;
+    
     useEffect(() =>{
         getArticles();
 
-       
+        // // console.log("Articles: " + articles)
+        // if(!articles)
+        // {
+        //     articleList = "no articles";
+        // }else{
+           
+        //         articleList = articles.forEach((article, index) => {
+        //             console.log(article, index);
+        //             articleList = <TestArticleItem article={article} index={index}/>
+        //         });
+
+        //         articleList = articles.map((article, k) => {
+        //             <TestArticleItem article={article} key={k}/>
+        //         })
+        
+        // }
+
+
     }, [])
 
   return (
     <div>
         <h1>Display</h1>
+        {articleList}
+        {/* {articles && 
+        articles.forEach((article, index) => {
+            <TestArticleItem article={article} index={index}/>
+        })} */}
     </div>
   )
 }
